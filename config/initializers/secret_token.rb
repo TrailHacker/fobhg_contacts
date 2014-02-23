d84017c5138da9ca7ce2c8ff0053751b33e3a995
TrailHacker <lucas@hardbarger.com>
2014-02-22 22:20:01 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FobhgContacts::Application.config.secret_key_base = '171ba22508a7bacf214110a5c8258b85e36a8d97f18478d6bf1cf62961e1775407f76f1422ae0e4fc294b0bdbb6f0adc6b151870d67d806e27e7804b4807a957'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FobhgContacts::Application.config.session_store :cookie_store, key: '_fobhg_contacts_session'
