d0f00cfafbe432e360ffb0e44e33444b74d988c6
TrailHacker <lucas@hardbarger.com>
2014-02-26 20:29:43 -0500

    Updated staging files for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,13 +34,18 @@ set :deploy_to, '/var/www/fobhg_contacts'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+before "deploy:setup", "rvm:install_rvm"
+before "deploy:setup", "rvm:import_gemset"
+after "deploy:restart", "deploy:cleanup"
+after "deploy:update_code", "deploy:migrate"
+
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -49,9 +54,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'cache:clear'
+      end
     end
   end
 
