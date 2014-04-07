f33f624e3b6b26efeea138ca6bce629fda5444de
TrailHacker <lucas@hardbarger.com>
2014-04-07 19:23:53 -0400

    Update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock '3.1.0'
 set :application, 'fobhg_contacts'
 set :repo_url, 'git@github.com:TrailHacker/fobhg_contacts.git'
 
+set :stages, ["staging", "production"]
+set :default_stage, "production"
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
@@ -34,25 +37,24 @@ set :deploy_to, '/var/www/fobhg_contacts'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      run "touch #{current_path}/tmp/restart.txt"
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
-      end
-    end
-  end
-
-end
+#namespace :deploy do
+#
+#  desc 'Restart application'
+#  task :restart do
+#    on roles(:app), in: :sequence, wait: 5 do
+#      # Your restart mechanism here, for example:
+#      run "touch #{current_path}/tmp/restart.txt"
+#    end
+#  end
+#
+#  after :publishing, :restart
+#
+#  after :restart, :clear_cache do
+#    on roles(:web), in: :groups, limit: 3, wait: 10 do
+#      # Here we can do anything such as:
+#      within release_path do
+#        execute :rake, 'cache:clear'
+#      end
+#    end
+#  end
+#end
